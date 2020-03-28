8dd817b19a0a4238c5c54ac0decef7f5087c2f4b
Sai Aung Naing Tun <saisaiant@gmail.com>
2020-03-28 16:34:33 +0630

    add auth link

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import Card from "../../shared/components/UIElements/Card";
 import Input from "../../shared/components/FormElements/Input";
 import Button from "../../shared/components/FormElements/Button";
@@ -8,10 +8,12 @@ import {
   VALIDATOR_REQUIRE
 } from "../../shared/util/validators";
 import { useForm } from "../../shared/hooks/form-hook";
+import { AuthContext } from "../../shared/context/auth-context";
 
 import "./Auth.css";
 
 const Auth = () => {
+  const auth = useContext(AuthContext);
   const [isLoginMode, setIsLoginMode] = useState(true);
 
   const [formState, inputHandler, setFormData] = useForm(
@@ -55,13 +57,14 @@ const Auth = () => {
   const authSubmitHandler = event => {
     event.preventDefault();
     console.log(formState.inputs);
+    auth.login();
   };
 
   return (
     <Card className="authentication">
-      <h2>Login Required</h2>
-      <hr />
+      <h2> Login Required </h2> <hr />
       <form onSubmit={authSubmitHandler}>
+        {" "}
         {!isLoginMode && (
           <Input
             element="input"
